dcbde58e9a7845ffdf559155c7527b854832d64b
PitaKibunja <pitakibunja@gmail.com>
2023-10-14 10:44:00 +0300

    added game-dashboard

--- app.js
+++ app.js
@@ -4,6 +4,24 @@ const port=3000
 
 app.set('view engine', 'pug')
 app.set('views','./views')
+const games = [
+    { name: 'Game 1', image: './images/game1.jpeg', description: 'Description for Game 1' },
+    { name: 'Game 2', image: './images/game2.jpeg', description: 'Description for Game 2' },
+    { name: 'Game 3', image: './images/game3.jpeg', description: 'Description for Game 3' },
+    { name: 'Game 4', image: './images/game4.jpeg', description: 'Description for Game 4' },
+    { name: 'Game 5', image: './images/game5.png', description: 'Description for Game 5' },
+    { name: 'Game 6', image: './images/game6.png', description: 'Description for Game 6' },
+    { name: 'Game 7', image: './images/game7.jpeg', description: 'Description for Game 7' },
+    { name: 'Game 8', image: 'https://i.pravatar.cc/150', description: 'Description for Game 8' },
+    { name: 'Game 9', image: 'https://i.pravatar.cc/150', description: 'Description for Game 9' },
+    // Add more game objects as needed
+];
+// Define the dummyPlayers array (an example)
+const dummyPlayers = [
+    { name: 'Player 1' },
+    { name: 'Player 2' },
+    // Add more players as needed
+  ];
 
 app.get('/', (req,res)=>{
     res.render('home')
@@ -15,7 +33,7 @@ app.get('/initial-profile', (req,res)=>{
     res.render('initial-profile')
 })
 app.get('/game-dashboard', (req,res)=>{
-    res.render('game-board')
+    res.render('dashboard',{ games, dummyPlayers})
 })
 app.get('/game-dashboard/profile', (req, res) => {
     // Assuming you have a user object defined here
